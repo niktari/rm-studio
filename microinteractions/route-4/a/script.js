afc52bb0984deacd7d0ddef0325c213fdff12c0c
Munus Shih <munusshih@newschool.edu>
2025-02-27 23:15:39 -0500

    Rename customCursor to selectCursor

--- microinteractions/route-4/a/script.js
+++ microinteractions/route-4/a/script.js
@@ -1,15 +1,15 @@
-const customCursor = document.querySelector("#custom-cursor");
+const viewMore = document.querySelector("#view-more");
 const selectDivClass = document.querySelectorAll(".project-card");
 let timeout;
 
 function animateText() {
-  const originalText = customCursor.innerHTML.trim();
-  customCursor.innerHTML = "";
+  const originalText = viewMore.innerHTML.trim();
+  viewMore.innerHTML = "";
 
   originalText.split("").forEach((char, index) => {
     let span = document.createElement("span");
     span.textContent = char;
-    customCursor.appendChild(span);
+    viewMore.appendChild(span);
 
     span.style.animationDelay = `${index * 0.3}s`;
   });
@@ -20,19 +20,19 @@ animateText();
 // Show cursor only on hover of viewmore elements
 selectDivClass.forEach(el => {
   el.addEventListener('mouseenter', () => {
-    customCursor.style.transform = "translate(-50%, -50%) scale(1)";
-    customCursor.style.opacity = "1";
+    viewMore.style.transform = "translate(-50%, -50%) scale(1)";
+    viewMore.style.opacity = "1";
     el.style.cursor = "pointer";
   });
 
   el.addEventListener('mousemove', (e) => {
-    customCursor.style.left = `${e.clientX}px`;
-    customCursor.style.top = `${e.clientY}px`;
+    viewMore.style.left = `${e.clientX}px`;
+    viewMore.style.top = `${e.clientY}px`;
   });
 
   el.addEventListener('mouseleave', () => {
-    customCursor.style.transform = "translate(-50%, -50%) scale(0.5)";
-    customCursor.style.opacity = "0";
+    viewMore.style.transform = "translate(-50%, -50%) scale(0.5)";
+    viewMore.style.opacity = "0";
     el.style.cursor = "default";
   });
 });
