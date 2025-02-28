df661d42104ca35794cf1b4428497847b3571643
Munus Shih <munusshih@newschool.edu>
2025-02-28 00:15:50 -0500

    Update script.js

--- microinteractions/route-4/a/script.js
+++ microinteractions/route-4/a/script.js
@@ -29,8 +29,8 @@ function handleHoverEffect(selectors) {
     });
 
     el.addEventListener('mousemove', (e) => {
-      viewMore.style.left = `${e.clientX}px`;
-      viewMore.style.top = `${e.clientY}px`;
+      viewMore.style.left = `${e.pageX}px`;
+      viewMore.style.top = `${e.pageY}px`;
     });
 
     el.addEventListener('mouseleave', () => {
