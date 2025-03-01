39aec516098fd1a92ad152c3bb74a7e0bc1569e2
niktari <77365760+niktari@users.noreply.github.com>
2025-03-01 01:33:21 -0500

    update scale

--- microinteractions/route-4/a/script.js
+++ microinteractions/route-4/a/script.js
@@ -10,7 +10,7 @@ function handleViewMore() {
     document.querySelectorAll(selector).forEach((el) => {
       el.addEventListener("mouseenter", () => {
         Object.assign(viewMore.style, {
-          transform: "translate(-50%, -100%) scale(1)",
+          transform: "translate(-50%, -100%)",
           opacity: "1",
         });
         el.style.cursor = "pointer";
@@ -24,7 +24,7 @@ function handleViewMore() {
       });
       el.addEventListener("mouseleave", () => {
         Object.assign(viewMore.style, {
-          transform: "translate(-50%, -100%) scale(0.5)",
+          transform: "translate(-50%, -100%)",
           opacity: "0",
         });
         el.style.cursor = "default";
